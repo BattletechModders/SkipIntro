c7d30ce7db03ec7639954c028da1e446d876ff50
Michael P. Starkweather <mpstark@gmail.com>
2018-06-01 18:45:10 -0500

    Initial commit of working code

---
+++ SkipIntro/Patches.cs
@@ -0,0 +1,27 @@
+﻿using System.Reflection;
+using BattleTech;
+using Harmony;
+
+// ReSharper disable InconsistentNaming
+// ReSharper disable UnusedMember.Global
+
+namespace SkipIntro
+{
+    [HarmonyPatch(typeof(IntroCinematicLauncher), "Init")]
+    public static class IntroCinematicLauncher_Init_Patch
+    {
+        public static void Postfix()
+        {
+            Traverse.Create(typeof(IntroCinematicLauncher)).Field("state").SetValue(3);
+        }
+    }
+
+    public static class Patches
+    {
+        public static void Init()
+        {
+            var harmony = HarmonyInstance.Create("io.github.mpstark.SkipIntro");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+}
