54c48c7fdd43f2c1778662a2840d4eb45a83bc94
Michael P. Starkweather <mpstark@gmail.com>
2018-06-20 20:56:20 -0500

    Add skipping of 1.1's new splash screens

--- SkipIntro/Patches.cs
+++ SkipIntro/Patches.cs
@@ -1,4 +1,7 @@
-﻿using System.Reflection;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
 using BattleTech;
 using Harmony;
 
@@ -15,7 +18,29 @@ namespace SkipIntro
             Traverse.Create(typeof(IntroCinematicLauncher)).Field("state").SetValue(3);
         }
     }
+    
+    [HarmonyPatch(typeof(SplashLauncher), "Update")]
+    public static class SplashLauncher_Update_Patch
+    {
+        public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            var codeInstructions = instructions.ToList();
+            foreach (var instruction in codeInstructions)
+            {
+                if (instruction.opcode == OpCodes.Call && instruction.operand.ToString().Contains("get_anyKeyDown"))
+                {
+                    instruction.opcode = OpCodes.Nop;
+                }
+                else if (instruction.opcode == OpCodes.Brfalse)
+                {
+                    instruction.opcode = OpCodes.Nop;
+                }
+            }
 
+            return codeInstructions;
+        }
+    }
+    
     public static class Patches
     {
         public static void Init()
