87a74e83dcd9819efd57c1fbba457ae5d2a6dc57
Michael P. Starkweather <mpstark@gmail.com>
2019-01-20 21:31:13 -0600

    Remove unneeded usings

--- SkipIntro/Main.cs
+++ SkipIntro/Main.cs
@@ -1,7 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Reflection.Emit;
+﻿using System.Reflection;
 using Harmony;
 using HBS.Logging;
 
--- SkipIntro/Patches/SplashLauncher.cs
+++ SkipIntro/Patches/SplashLauncher.cs
@@ -1,7 +1,4 @@
 ﻿using Harmony;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection.Emit;
 
 namespace SkipIntro
 {
