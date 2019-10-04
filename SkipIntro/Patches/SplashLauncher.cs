6ee54c9e06e95b03bd6d1d053694dc7331f46971
Michael P. Starkweather <mpstark@gmail.com>
2019-10-03 20:51:53 -0500

    Change SplashLauncher patch to work with new hook point

--- SkipIntro/Patches/SplashLauncher.cs
+++ SkipIntro/Patches/SplashLauncher.cs
@@ -5,43 +5,13 @@
 
 namespace SkipIntro.Patches
 {
-    [HarmonyPatch(typeof(SplashLauncher), "OnStart")]
-    public static class SplashLauncher_OnStart_Patch
-    {
-        public static bool Prefix()
-        {
-            return false;
-        }
-    }
-
-    [HarmonyPatch(typeof(SplashLauncher), "OnStep")]
-    public static class SplashLauncher_OnStep_Patch
-    {
-        public static bool Prefix()
-        {
-            return false;
-        }
-    }
-
     [HarmonyPatch(typeof(SplashLauncher), "Update")]
     public static class SplashLauncher_Update_Patch
     {
-        public static bool Prefix()
-        {
-            return false;
-        }
-    }
-
-    [HarmonyPatch(typeof(SplashLauncher), "Start")]
-    public static class SplashLauncher_Start_Patch
-    {
         public static bool Prefix(SplashLauncher __instance)
         {
-            Traverse.Create(__instance).Field("currentState").SetValue(3);
-
             var activate = Traverse.Create(__instance).Field("activate").GetValue<ActivateAfterInit>();
             activate.enabled = true;
-
             return false;
         }
     }
